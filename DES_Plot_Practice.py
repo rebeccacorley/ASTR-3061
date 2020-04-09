c2c9ca94a2ca25be9ffb0de4c4c9e59a248f6c66
Rebecca Corley <46977097+rebeccacorley@users.noreply.github.com>
2020-04-09 13:10:13 -0400

    Create DES_Plot_Practice.py
    Hi! I am playing around trying to figure out how to edit/compare files and code!!

---
+++ DES_Plot_Practice.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 15:03:51 2020
+@author: rebeccacorley
+"""
+
+# This is my first attempt in plotting redshift vs. log distance whoo.  
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt('/Users/rebeccacorley/Documents/Observational Astro/DES-Data1.txt')
+print(data)
+
+redshift = data[:,4] #columns sart at 0!!
+log_dist = data[:,2] #however many columns I want, the colon indicates all 
+ 
+
+#The following lines play around with different graph properties
+plt.plot(redshift, log_dist, '.', ms = 5, color = 'k', label = 'redshift data')
+plt.xlabel('Z')
+plt.ylabel('log(d)') 
+plt.legend()
+plt.show()
